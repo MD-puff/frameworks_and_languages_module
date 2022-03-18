7a513c645e578e41a438167b6ec9e8d4573cfea5
Martynas Dirvonskis <md597@canterbury.ac.uk>
2022-03-18 09:48:14 +0000

    Installed cors, created get, post for item

--- server/app.js
+++ server/app.js
@@ -1,14 +1,38 @@
 // from https://expressjs.com/en/starter/hello-world.html
 
-const express = require('express')
-const app = express()
-const port = 8000 //server port
+const express = require('express');
+const app = express();
+const port = 8000; //server port
+const cor = require('cors')
+const path = require('path');
+const items = require('./itemStorage');
 
-app.get('/', (req, res) => {
-  //res.send('Hello World!')
+app.use(cors())
 
-  // https://expressjs.com/en/4x/api.html#res.json
-  res.json({ hello: 'world' })
+
+//Route
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.js'))
+})
+
+app.post("/items", (req, res) => {
+  var nextId = parseInt(Object.keys(items).reduce((a, b) => items[a] > items[b] ? a : b)) + 1;//Finds a unused number to identify item 
+  const newItem = {//
+    id: nextId,//int
+    user_id: req.body.user_id,//string
+    keywords: req.body.keywords,//array
+    description: req.body.description,//string
+    image: req.body.image,//string
+    lat: req.body.latitude,//int
+    lon: req.body.longitude,//int
+    date_from: new Date,//date
+    date_to: new Date//date
+  }
+  if (!newItem.user_id || !newItem.keywords || !newItem.description || !newItem.lat || !newItem.lon) {//reject if a field is empty
+    return res.status(405).json({ msg: 'Invalid input' });
+  }
+  items[newItem.id] = newItem;
+  res.status(201).json(items);
 })
 
 app.listen(port, () => {
@@ -25,4 +49,4 @@ app.use(function (err, req, res, next) {
 // https://github.com/nodejs/node/issues/4182
 process.on('SIGINT', function() {
     process.exit();
-});
+})
